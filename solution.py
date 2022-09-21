18434092f062538be089f4854fce4f335d6bcd12
chidi-udeze <only4image@yahoo.com>
2022-09-21 17:59:45 -0400

    code modify

--- solution.py
+++ solution.py
@@ -22,7 +22,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # end
 
     # Send HELO command and print server response.
-    heloCommand = 'HELO Chidi\r\n'
+    heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
     # to cmnt
